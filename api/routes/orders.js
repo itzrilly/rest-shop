9a22d71b44f3124c12d8ce9012d4e24e7191e037
Rilwanou Garba <rilwanougarbarg@gmail.com>
2023-03-19 20:33:09 +0100

    Populating Queries with Mongoose | Creating a REST API with Node.js

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -7,7 +7,7 @@ const Product = require('../models/product')
 
 // Handle incoming GET requests to /orders
 router.get('/', (req, res, next) => {
-    Order.find().select('_id product quantity').exec().then(docs => {
+    Order.find().select('_id product quantity').populate('product', 'name').exec().then(docs => {
         res.status(200).json({
             count: docs.length,
             orders: docs.map(doc => {
@@ -67,7 +67,7 @@ router.post('/', (req, res, next) => {
 })
 
 router.get('/:orderId', (req, res, next) => {
-    Order.findById(req.params.orderId).select('_id product quantity').exec().then(order => {
+    Order.findById(req.params.orderId).select('_id product quantity').populate('product').exec().then(order => {
         if(!order){
             return res.status(404).json({
                 message: 'Order not found'
