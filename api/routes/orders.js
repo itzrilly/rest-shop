e5b8ccbcdd8492cb76cb2aaf07732b936d8110d8
Rilwanou Garba <rilwanougarbarg@gmail.com>
2023-04-06 12:19:43 +0100

    JWT Route Protection | Creating a REST API with Node.js

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
+const checkAuth = require('../middleware/check-auth')
 
 const Order = require('../models/order')
 const Product = require('../models/product')
 
 // Handle incoming GET requests to /orders
-router.get('/', (req, res, next) => {
+router.get('/', checkAuth, (req, res, next) => {
     Order.find().select('_id product quantity').populate('product', 'name').exec().then(docs => {
         res.status(200).json({
             count: docs.length,
@@ -29,7 +30,7 @@ router.get('/', (req, res, next) => {
     })
 })
 
-router.post('/', (req, res, next) => {
+router.post('/', checkAuth, (req, res, next) => {
     Product.findById(req.body.productId).then(product => {
         if (!product) {
             return res.status(404).json({
@@ -66,7 +67,7 @@ router.post('/', (req, res, next) => {
     })
 })
 
-router.get('/:orderId', (req, res, next) => {
+router.get('/:orderId', checkAuth, (req, res, next) => {
     Order.findById(req.params.orderId).select('_id product quantity').populate('product').exec().then(order => {
         if(!order){
             return res.status(404).json({
@@ -87,7 +88,7 @@ router.get('/:orderId', (req, res, next) => {
     })
 })
 
-router.delete('/:orderId', (req, res, next) => {
+router.delete('/:orderId', checkAuth, (req, res, next) => {
     Order.remove({ _id: req.params.orderId }).exec().then(result => {
         res.status(200).json({
             message: 'Order deleted',
